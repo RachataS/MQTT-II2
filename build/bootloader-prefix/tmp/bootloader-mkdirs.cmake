a8961d23dbde355169a640812d808b79ed3147c3
RachataS <64030161@kmitl.ac.th>
2023-10-02 18:43:54 +0700

    commit

---
+++ build/bootloader-prefix/tmp/bootloader-mkdirs.cmake
@@ -0,0 +1,19 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/Users/rachatasupanurak/esp/esp-idf/components/bootloader/subproject"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix/tmp"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix/src/bootloader-stamp"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix/src"
+  "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix/src/bootloader-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/Users/rachatasupanurak/MQTT-II2/build/bootloader-prefix/src/bootloader-stamp/${subDir}")
+endforeach()
